398bddffab590cfaff3202187851849e83fa5f40
alifakbar111 <alifakbar111@gmail.com>
2022-06-27 11:17:47 +0700

    feat: cart component

--- src/sections/cart.tsx
+++ src/sections/cart.tsx
@@ -1,4 +1,13 @@
-import { VStack } from '@chakra-ui/react';
+import {
+  Heading,
+  VStack,
+  Text,
+  Button,
+  HStack,
+  AspectRatio,
+  Image,
+  Stack,
+} from '@chakra-ui/react';
 
 const Cart = () => {
   return (
@@ -10,7 +19,55 @@ const Cart = () => {
       alignItems="flex-start"
       bg="gray.50"
     >
-      <h1>Heloo</h1>
+      <Heading size="lg">Your cart</Heading>
+      <Text>
+        If price is too hard on your eyes
+        <Button variant="link"> try changing the theme.</Button>
+      </Text>
+      <HStack spacing={6} alignItems="center" w="full">
+        <AspectRatio ratio={1} w={24}>
+          <Image src="/images/skateboard.jpg" alt="Skateboard" />
+        </AspectRatio>
+        <Stack
+          spacing={0}
+          w="full"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <VStack w="full" spacing={0} alignItems="flex-start">
+            <Heading size="md">Penny board</Heading>
+            <Text color="gray.600">PNYCOMP27541</Text>
+          </VStack>
+          <Heading size="sm" textAlign="end">
+            $119.00
+          </Heading>
+        </Stack>
+      </HStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color="gray.600">Subtotal</Text>
+          <Heading size="sm">$119.00</Heading>
+        </HStack>
+      </VStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color="gray.600">Shipping</Text>
+          <Heading size="sm">$19.99</Heading>
+        </HStack>
+      </VStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color="gray.600">Taxes (estimated)</Text>
+          <Heading size="sm">$23.80</Heading>
+        </HStack>
+      </VStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color="gray.600">Total</Text>
+          <Heading size="lg">$162.79</Heading>
+        </HStack>
+      </VStack>
     </VStack>
   );
 };
